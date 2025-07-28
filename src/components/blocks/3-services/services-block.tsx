4e15f28f498456c38d1bbb84246fee8d2bb87c9b
Kirill <goodhumored@vk.com>
2025-07-29 01:37:44 +0300

    services block finished (no anim yet)

--- src/components/blocks/3-services/services-block.tsx
+++ src/components/blocks/3-services/services-block.tsx
@@ -8,9 +8,7 @@ export default function ServicesBlock({ className }: { className?: string }) {
   return (
     <section id="services" className={cn("relative w-full overflow-hidden lg:pt-35 pt-10 md:pt-20 fluid-container", className)}>
       <SectionHeader>Services</SectionHeader>
-      {/* <div className="mt-7.5 w-full flex justify-between flex-wrap"> */}
-      <div className="text-justify lg:mt-7.5 mt-2.5 w-full font-medium font-cera break-keep break-words leading-[1.25] @container">
-        {/* Corporate identity, Product identity, Campaign identity, Personal identity, Brand support, Naming, Logo, Packaging, Brand guidelines, UI/⁠UX, Key Visual, Content design */}
+      <div className="text-justify md:mt-7.5 mt-2.5 w-full font-medium font-cera break-keep break-words leading-[1.25] @container">
         {
           services.map((service, index) => (
             <Link href={`#services`} key={index} className="text-[5.71cqw] font-medium font-cera inline not-last:after:[content:',_']">
