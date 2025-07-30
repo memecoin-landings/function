ec67f826e024b5f0c015fb1435fd3c815abe89ac
Kirill <goodhumored@vk.com>
2025-07-30 21:31:48 +0300

    fix package.json, fix subheader, fix container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import HomeBlock from "@/components/blocks/1-home/home-block";
+import ProjectsBlock from "@/components/blocks/2-projects/projects-block";
 import ServicesBlock from "@/components/blocks/3-services/services-block";
 
 export const dynamic = "force-dynamic";
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center sm:items-start">
       <HomeBlock />
+      <ProjectsBlock />
       <ServicesBlock />
     </main>
   );
--- src/components/common/section-header.tsx
+++ src/components/common/section-header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function SectionHeader({ className, children }: { className?: string, children?: React.ReactNode }) {
   return (
-    <h2 className={`${className} font-cera md:text-3xl xs:text-base text-xl text-[#727272]`}>
+    <h2 className={`${className} font-cera md:text-3xl font-medium xs:text-base text-xl text-[#727272]`}>
       {children}
     </h2>
   )
