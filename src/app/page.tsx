ce904c44706b3a15db605cb8f343480b31e41f3e
WCoopW <artemmonakov@rambler.ru>
2025-07-31 01:44:16 +0300

    feat: add projects block on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center sm:items-start md:pt-25.75 xs:pt-18.25 pt-17">
       <HomeBlock className="" />
+      <ProjectsBlock className="md:pt-32.75 xs:pt-18.25 pt-17" />
       <ServicesBlock />
       <ClientsBlock className="md:mt-42.5 xs:mt-25 mt-17.5" />
       <FedorBlock />
