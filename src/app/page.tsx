011f364a454b7f13d34af48306c7e32f0cfb9c2f
WCoopW <artemmonakov@rambler.ru>
2025-07-31 01:27:50 +0300

    fix paddings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center sm:items-start">
-      <HomeBlock mt-37 />
+    <main className="flex flex-col items-center sm:items-start md:pt-25.75 xs:pt-18.25 pt-17">
+      <HomeBlock className="" />
       <ServicesBlock />
     </main>
   );
--- src/components/blocks/1-home/home-block.tsx
+++ src/components/blocks/1-home/home-block.tsx
@@ -1,15 +1,15 @@
-export default function HomeBlock() {
+import { cn } from "@/lib/utils";
+
+export default function HomeBlock({ className }: { className?: string }) {
   return (
-    <section className="relative w-full lg:pt-35 pt-10 md:pt-20">
-      <div className="flex flex-col">
-        <div className="@container">
-          <div className="text-[#FF3F1A] font-bold leading-[16cqw] tracking-[-3%] text-[18.7cqw] whitespace-nowrap">
-            Unique
-            <br />
-            Solutions —
-          </div>
+    <section className={cn("relative w-full", className)}>
+      <div className="flex flex-col @container">
+        <div className="text-[#FF3F1A] font-bold leading-[16cqw] tracking-[-3%] text-[18.7cqw] whitespace-nowrap pl-2.5 xs:pl-0">
+          Unique
+          <br />
+          Solutions —
         </div>
-        <p className="text-5xl @container text-[3.75cqw] leading-[4.5cqw] mt-[-1.51em] relative z-10 font-medium pl-5 pr-36">
+        <p className="text-5xl @container text-[3.75cqw] leading-[4.5cqw] mt-[-1.51em] relative z-10 font-medium md:pl-5 pl-2.5 md:pr-[9.58cqw] xs:px-2.5">
           Functionally and strategically refined design by a brand identity
           studio that solves business challenges, drives growth, and is based on
           in‑depth analysis
