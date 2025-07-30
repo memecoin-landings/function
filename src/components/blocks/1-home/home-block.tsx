9489876332e6c2b6c4d9ecddd96cdd37ce049c9d
WCoopW <artemmonakov@rambler.ru>
2025-07-31 00:03:25 +0300

    fix: change weight on text. Add left padding

--- src/components/blocks/1-home/home-block.tsx
+++ src/components/blocks/1-home/home-block.tsx
@@ -9,10 +9,7 @@ export default function HomeBlock() {
             Solutions —
           </div>
         </div>
-        <p
-          className="text-5xl @container text-[4cqw] mt-[-1em] relative z-10"
-          style={{ marginTop: "-1.4em" }}
-        >
+        <p className="text-5xl @container text-[4cqw] mt-[-1.3em] relative z-10 font-medium pl-5">
           Functionally and strategically refined design by a brand identity
           studio that solves business challenges, drives growth, and is based on
           in‑depth analysis
