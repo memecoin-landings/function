5f451d017353e738808cf959f156ee0406f178d1
Kirill <goodhumored@vk.com>
2025-07-31 01:58:16 +0300

    feat: projects hover cursorfalse

--- src/components/blocks/2-projects/projects-block.tsx
+++ src/components/blocks/2-projects/projects-block.tsx
@@ -55,13 +55,14 @@ export default function ProjectsBlock({ className }: { className?: string }) {
       <div className="grid xs:grid-cols-2 grid-cols-1 xs:gap-2.5 gap-1.5 xs:mt-7.5 mt-2.5">
         {infoBlocks.map((item, i) => (
           <div
+            custom-cursor="hover"
             key={i}
             className={`transition-all duration-700 ease-out 
               group-[.revealed]/scroll-reveal:opacity-100 group-[.revealed]/scroll-reveal:translate-y-0
               group-[.unrevealed]/scroll-reveal:opacity-0 group-[.unrevealed]/scroll-reveal:translate-y-10`}
             style={{ transitionDelay: `${i * 150}ms` }}
           >
-            <BgImageCard className="w-full" data={item} />
+            <BgImageCard className="w-full " data={item} />
           </div>
         ))}
       </div>
