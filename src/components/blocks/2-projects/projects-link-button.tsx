e05a9595d065727880f5e4b6f0e0257d0c195453
WCoopW <artemmonakov@rambler.ru>
2025-07-31 01:36:14 +0300

    feat: add transition to arrom link. Fix arrow hover area

--- src/components/blocks/2-projects/projects-link-button.tsx
+++ src/components/blocks/2-projects/projects-link-button.tsx
@@ -2,11 +2,13 @@ import ArrowSVG from "@/components/common/arrow";
 
 export default function ProjectLink() {
   return (
-    <div className="group flex justify-end items-center py-5 cursor-pointer">
-      <span className="group-hover:text-[#FF3F1A] text-lg font-bold mr-5">
-        All Projects
-      </span>
-      <ArrowSVG className="fill-white group-hover:fill-[#FF3F1A]"></ArrowSVG>
+    <div className="flex justify-end py-5 cursor-pointer">
+      <div className="group items-center flex-row flex">
+        <span className="group-hover:text-[#FF3F1A] text-lg font-bold mr-5 transition-colors duration-300">
+          All Projects
+        </span>
+        <ArrowSVG className="fill-white group-hover:fill-[#FF3F1A] transition-colors duration-300"></ArrowSVG>
+      </div>
     </div>
   );
 }
