41e0ac6136170463cfe9946263dca5e928fa9ac5
Kirill <goodhumored@vk.com>
2025-08-03 15:42:51 +0300

    cursor framer-motion to animejs

--- src/components/blocks/1-home/home-block.tsx
+++ src/components/blocks/1-home/home-block.tsx
@@ -2,17 +2,15 @@ import { cn } from "@/lib/utils";
 
 export default function HomeBlock({ className }: { className?: string }) {
   return (
-    <section className={cn("relative w-full", className)}>
-      <div className="flex flex-col @container">
-        <div className="text-[#FF3F1A] font-bold leading-[16cqw] tracking-[-3%] text-[18.7cqw] whitespace-nowrap pl-2.5 xs:pl-0">
-          Unique
-          <br />
-          Solutions —
-        </div>
-        <p className="text-5xl @container text-[3.75cqw] leading-[4.5cqw] mt-[-1.51em] relative z-10 font-medium md:pl-5 pl-2.5 md:pr-[9.58cqw] xs:px-2.5">
-          Functionally and strategically refined design by a brand identity studio that solves business challenges, drives growth, and is based on in‑depth analysis
-        </p>
+    <section className={cn("relative w-full flex flex-col @container fluid-container", className)}>
+      <div className="text-[#FF3F1A] font-bold leading-[16cqw] tracking-[-3%] text-[18.7cqw] whitespace-nowrap  xs:pl-0">
+        Unique
+        <br />
+        Solutions —
       </div>
+      <p className="text-5xl @container text-[3.75cqw] leading-[4.5cqw] mt-[-1.51em] relative z-10 font-medium">
+        Functionally and strategically refined design by a brand identity studio that solves business challenges, drives growth, and is based on in‑depth analysis
+      </p>
     </section>
   );
 }
