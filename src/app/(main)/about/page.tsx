22ad041dba377ff92939d618c8f4e3f527d4816d
klutrem <danyaryabov@mail.ru>
2025-09-14 18:53:01 +0300

    feat: about

---
+++ src/app/(main)/about/page.tsx
@@ -0,0 +1,13 @@
+import ServicesBlock from "@/components/blocks/3-services/services-block";
+import ClientsBlock from "@/components/blocks/4-clients/clients-block";
+import StrategicIdentityBlock from "@/components/blocks/1-strategic-identity/page";
+
+export default function AboutPage() {
+  return (
+    <main className="flex flex-col items-center sm:items-start md:pt-25.75 xs:pt-18.25 pt-17.5 pb-25.75 md:pb-51.25 xs:pb-26">
+      <StrategicIdentityBlock />
+       <ClientsBlock className="md:mt-42.5 xs:mt-25 mt-17.5" />
+       <ServicesBlock className="md:mt-39 xs:mt-18 mt-10.5" />
+    </main>
+  );
+}
---
+++ src/components/blocks/1-strategic-identity/page.tsx
@@ -0,0 +1,10 @@
+export default function StrategicIdentityBlock() {
+  return (
+    <div>
+      <h1 className="text-[#FF3F1A] font-bold leading-[16cqw] tracking-[-3%] text-[18.7cqw] xs:pl-0"
+      >Strategic Identity</h1>
+      <h2 className="text-[#F0EDE8] font-medium leading-16.5 tracking-[-3%] text-[3.375rem] xs:pl-0"
+      >The studio&apos;s mission is to create functional and aesthetically refined brand identities that address key business challenges, drive growth, and open new markets. Combining analytics, art, and creativity, we deliver unique solutions that achieve tangible results.</h2>
+    </div>
+  );
+}
