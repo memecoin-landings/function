0b529174c6365f6af40222576fe7aef54430d6d7
klutrem <danyaryabov@mail.ru>
2025-10-07 01:17:07 +0300

    fix: strategic identity font sizes (xs only)

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -19,9 +19,9 @@ export default function AboutPage() {
     formRef.current?.focus();
   };
   return (
-    <main className="flex flex-col items-center sm:items-start md:pt-25.75 xs:pt-18.25 pt-17.5 pb-25.75 md:pb-51.25 xs:pb-26">
-      <StrategicIdentityBlock />
-      <FedorAboutBlock className="md:mt-42.5 xs:mt-25 mt-17.5" openModal={openModal} />
+    <main className="flex flex-col items-center sm:items-start ">
+      <StrategicIdentityBlock className="md:mt-25.75 xs:mt-18.25 mt-17.5" />
+      <FedorAboutBlock className="md:mt-50 xs:mt-25 mt-17.5" openModal={openModal} />
       <ClientsBlock className="md:mt-42.5 xs:mt-25 mt-17.5" />
       <ServicesBlock className="md:mt-39 xs:mt-18 mt-10.5" />
 
