39611c3fa4a52ca29178de54449429b3a36f1049
klutrem <danyaryabov@mail.ru>
2025-10-08 00:29:25 +0300

    about page responsive finished

--- src/components/blocks/2-fedor-about/fedor-about-block.tsx
+++ src/components/blocks/2-fedor-about/fedor-about-block.tsx
@@ -36,18 +36,18 @@ export default function FedorAboutBlock({ className, openModal }: { className?: 
     >
       <div className="@container">
         {/* Image with overlaid title */}
-        <div className="relative w-full mb-12 xs:mb-8 md:mb-7.5">
+        <div className="relative w-full mb-2.5 xs:mb-8 md:mb-7.5">
           <Image
             ref={pushIfNotNull(elementsRef.current)}
             src={fedorImage}
             alt="Fedor Beltugov"
-            className="object-cover object-center w-full xs:w-[42.25cqw] rounded-[0.313rem] grayscale hover:grayscale-0 transition-[filter] duration-350 ease-in px-4 xs:pl-18.25 xs:px-0"
+            className="object-cover object-center xs:w-[42.25cqw] w-[38.37cqw] h-full rounded-[0.313rem] grayscale hover:grayscale-0 transition-[filter] duration-350 ease-in ml-11.25 xs:ml-18.25"
             layout="cover"
           />
 
           {/* Overlaid title */}
           <h1
-            className="text-white font-cera font-bold md:text-[11.81cqw] xs:text-[11.5cqw] text-[11.63cqw] md:leading-[9.03cqw] leading-none tracking-[-3%] mt-[-0.5em] xs:mt-[-0.82em] xs:ml-2.5 relative z-10 text-left "
+            className="text-white font-cera font-bold md:text-[11.81cqw] xs:text-[11.5cqw] text-[10.5cqw] md:leading-[9.03cqw] leading-none tracking-[-3%] md:mt-[-0.69em] xs:mt-[-0.82em] mt-[-0.81em] md:ml-0 xs:ml-2.5 ml-2.5 xs:mr-0 mr-5.25 relative z-10 text-left"
           > Behind the&nbsp;Studio
           </h1>
         </div>
@@ -57,9 +57,9 @@ export default function FedorAboutBlock({ className, openModal }: { className?: 
           {/* Description */}
           <div
             ref={pushIfNotNull(elementsRef.current)}
-            className="mb-8 xs:mb-7.5"
+            className="mb-2.5 xs:mb-7.5"
           >
-            <p className="font-cera font-normal not-italic md:text-[2.08cqw] xs:text-[2.60cqw] text-[3.26cqw] md:leading-[2.71cqw] xs:leading-[3.39cqw] leading-[4.42cqw] tracking-[0%] text-[#F0EDE8] text-left px-4 xs:pl-2.5 xs:pr-34">
+            <p className="font-cera font-normal not-italic md:text-[2.08cqw] xs:text-[2.60cqw] text-[3.26cqw] md:leading-[2.71cqw] xs:leading-[3.39cqw] leading-[4.42cqw] tracking-[0%] text-[#F0EDE8] text-left md:pl-5 xs:pl-2.5 pl-2.5 md:pr-[23.4375rem] xs:pr-34 pr-11.25">
             Fedor Beltyugov, the founder of Function Design Studio, is an art director, designer, and strategist. Since 2008, I’ve been crafting brand identities that drive business goals and elevate brand value. I personally oversee every project from concept to completion. My experience spans working with global corporations, local businesses, and personal brands.
             </p>
           </div>
@@ -67,7 +67,7 @@ export default function FedorAboutBlock({ className, openModal }: { className?: 
           {/* CTA Links */}
           <div
             ref={pushIfNotNull(elementsRef.current)}
-            className="flex flex-col xs:flex-row gap-3 xs:gap-2 justify-left items-left px-4 xs:pl-2.5"
+            className="flex flex-row xs:flex-row gap-1.5 xs:gap-2 justify-left items-left md:pl-5 xs:pl-2.5 pl-2.5 md:pr-0 xs:pr-0 pr-2.5"
           >
             <button
               onClick={openModal}
